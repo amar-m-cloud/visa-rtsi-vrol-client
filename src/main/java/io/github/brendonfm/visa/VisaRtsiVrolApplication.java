2030ad5d0eb5d263a06d0866648c1ba81ba141cd
brendonfm <freitas.brendon@gmail.com>
2019-11-01 22:09:41 +0000

    initial commit

---
+++ src/main/java/io/github/brendonfm/visa/VisaRtsiVrolApplication.java
@@ -0,0 +1,13 @@
+package io.github.brendonfm.visa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaRtsiVrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaRtsiVrolApplication.class, args);
+	}
+
+}
